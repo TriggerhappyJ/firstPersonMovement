5e1e7c40ce428368eabf010c3a7503f224bed7bc
TriggerhappyJ <jacob30wheeler@gmail.com>
2023-05-20 13:14:46 +1200

    Moved platform interaction to correct script

--- Assets/Scripts/PlayerPlatformInteraction.cs
+++ Assets/Scripts/PlayerPlatformInteraction.cs
@@ -5,18 +5,57 @@ using UnityEngine;
 
 public class PlayerPlatformInteraction : MonoBehaviour
 {
+    private Rigidbody rBody;
+    private PlatformMove platMove;
+    
+    private bool playerIsTouching;
+    private PlayerMovement pMovement;
+    private Vector3 oldPosition;
+    [HideInInspector] public Vector3 platformVelocity;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        platMove = GetComponent<PlatformMove>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Calculate platforms velocity
+        var newPosition = rBody.position;
+        var platformDifference = newPosition - oldPosition;
+        platformVelocity = platformDifference / Time.fixedDeltaTime;
+        
+        oldPosition = newPosition;
+        
+        if (playerIsTouching && pMovement)
+        {
+            Debug.Log("Sending velocity! "+platformVelocity);
+            pMovement.addPlatformVelocity(platformVelocity);
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Player")
+        if (!other.gameObject.CompareTag("Player")) return;
+        
+        other.transform.parent = transform;
+        
+        if (pMovement == null)
         {
-            // Make the player a child of the platform
-            other.transform.parent = transform;
+            pMovement = other.transform.GetComponent<PlayerMovement>();
         }
+        
+        playerIsTouching = true;
     }
 
     private void OnCollisionExit(Collision other)
     {
-            // Remove the player as a child of the platform
-            other.transform.parent = null;
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        pMovement.addPlatformVelocity(Vector3.zero);
+        playerIsTouching = false;
+        
+        other.transform.parent = null;
     }
 }
